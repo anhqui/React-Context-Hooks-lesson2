ea4ccc05ff5ffde169f604ed1940cf923e062c34
Qui Nguyen <qui@gmail.com>
2024-02-25 23:00:16 -0500

    first commit

---
+++ src/components/SongList.jsx
@@ -0,0 +1,13 @@
+const SongList = () => {
+  return (
+    <div className="song-list">
+      <ul>
+        <li>Like a Rolling Stone</li>
+        <li>My Heart Will Go On</li>
+        <li>Lemon Tree</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SongList;
