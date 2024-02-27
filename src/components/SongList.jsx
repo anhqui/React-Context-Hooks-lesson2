abb5acb2d098d7c1b7c65b5ad7d8c4252ab9e557
Qui Nguyen <qui@gmail.com>
2024-02-26 21:53:21 -0500

    first commit

--- src/components/SongList.jsx
+++ src/components/SongList.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "Like a Rolling Stone", id: 1 },
+    { title: "My Heart Will Go On", id: 2 },
+    { title: "Lemon Tree", id: 3 },
+    { title: "My Love", id: 4 },
+  ]);
+  const addSong = () => {
+    // const newSongs = songs.push({ title: "new song", id: 5 });
+    // const newSongs = [...songs, { title: "new song", id: uuidv4() }];
+    setSongs([...songs, { title: "new song", id: uuidv4() }]);
+  };
+
   return (
     <div className="song-list">
       <ul>
-        <li>Like a Rolling Stone</li>
-        <li>My Heart Will Go On</li>
-        <li>Lemon Tree</li>
+        {songs.map((song) => (
+          <li key={song.id}>{song.title}</li>
+        ))}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
   );
 };
